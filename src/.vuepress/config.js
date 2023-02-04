1f0a690d83d46845355d334743efe7b5da7e92b2
artemshar <artemrandom@ya.ru>
2023-02-04 23:04:19 +0300

    add link about React.FC

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -103,6 +103,7 @@ module.exports = {
           collapsable: false,
           children: [
             '',
+            'commoon',
             'ui-libs',
             'state-management',
             'hooks',
---
+++ src/react/common.md
@@ -0,0 +1,4 @@
+## Writing React components
+
+### React.FC pattern (spoiler: don't use)
+- https://github.com/facebook/create-react-app/pull/8177
